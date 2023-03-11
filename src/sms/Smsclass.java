e53380f3bddebe232c85eb5b76a16a6610c0a44b
Praveen kumar K B <praveenkb2796@gmail.com>
2023-03-11 15:20:24 +0530

    This is commit from Asian timezone guy

---
+++ src/sms/Smsclass.java
@@ -0,0 +1,15 @@
+package sms;
+
+public class Smsclass {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is SMS Class");
+		System.out.println("Commit to git sucessfuly");
+		System.out.println("Push to github sucessfuly");
+						
+
+	}
+
+}

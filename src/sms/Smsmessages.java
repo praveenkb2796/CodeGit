bbee699baefee9715c3e35ae6de10dde37c358d5
Praveen kumar K B <praveenkb2796@gmail.com>
2023-03-13 17:54:33 +0530

    final commit by Asian development team

---
+++ src/sms/Smsmessages.java
@@ -0,0 +1,15 @@
+package sms;
+
+public class Smsmessages {
+
+	public static void main(String[] args) {
+		
+	
+		System.out.println("Sms Messages1");
+		System.out.println("Sms Messages2");
+		System.out.println("Sms Messages3");
+		
+
+	}
+
+}
